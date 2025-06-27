55958d68a4e35c0495fc599733deb795209d405c
ígorpetriv.bsc <igorpetriv.bsc@gmail.com>
2025-06-28 00:34:10 +0300

    done

--- app/NotFound.client.tsx
+++ app/NotFound.client.tsx
@@ -1,24 +1,38 @@
 "use client";
 import css from "./NotFound.module.css";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const NotFoundClient = () => {
   const router = useRouter();
 
+  const [countdown, setCountdown] = useState(5);
+
   useEffect(() => {
-    const timer = setTimeout(() => router.push("/"), 5000);
-    return () => clearTimeout(timer);
+    const interval = setInterval(() => {
+      setCountdown((prev) => prev - 1);
+    }, 1000);
+
+    const timer = setTimeout(() => {
+      router.push("/");
+    }, 5000);
+
+    return () => {
+      clearInterval(interval);
+      clearTimeout(timer);
+    };
   }, [router]);
 
   return (
-    <div className={css.container}>
+    <>
+      <div className={css.container}></div>
       <h1 className={css.title}>404 - Page not found</h1>
       <p className={css.description}>
         Sorry, the page you are looking for does not exist. <br />
-        You will be redirected to the homepage in 5 seconds.
+        You will be redirected to the homepage in {}
+        {countdown > 0 ? countdown : 0} seconds.
       </p>
-    </div>
+    </>
   );
 };
 
